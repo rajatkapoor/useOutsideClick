2ca352d188a6035b22121288332575d9fa74b88f
Rajat Kapoor <rajat100493@gmail.com>
2021-05-09 20:14:47 +0530

    Added clickOutside functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useEffect, useRef } from "react";
 import "./styles.css";
 
 export default function App() {
+  const mainRef = useRef();
+  const onOutsideClick = (e) => {
+    const inMain = mainRef.current.contains(e.target);
+    const isOutside = !inMain;
+    if (isOutside) {
+      console.log("Clicked ouside");
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", onOutsideClick);
+    return () => {
+      console.log("cleanup");
+      document.removeEventListener("click", onOutsideClick);
+    };
+  }, [mainRef]);
   return (
     <div className="App">
-      <div className="main">Click me</div>
+      <div className="main" ref={mainRef}>
+        Click me
+      </div>
     </div>
   );
 }
